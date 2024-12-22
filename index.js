4ac92d5ee2100a28039bb6f262edf919c0d96f39
Md Mozammil Aman <amanmozammil.07@gmail.com>
2024-12-22 12:14:32 +0530

    added schema

--- index.js
+++ index.js
@@ -8,5 +8,10 @@ main()
 
 async function main() {
   await mongoose.connect('mongodb://127.0.0.1:27017/test');
-
 }
+
+const userSchema = new mongoose.Schema({
+    name: String,
+    email: String,
+    age: Number,
+});
